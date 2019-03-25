3a5840e343888ddf5e7d5267d4b8de895a555b81
whoami-I <1874785601@qq.com>
2019-03-25 09:58:02 +0800

    添加变色字体 自定义View ColorTrackTextView

---
+++ src/com/example/selfdefineview/activity/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.selfdefineview.activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+import com.example.selfdefineview.QQStepView;
+import com.example.selfdefineview.R;
+
+class MyScrollView extends ScrollView {
+
+	public MyScrollView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.setFillViewport(true);
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		System.out.println(super.isFillViewport());
+
+	}
+
+}
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_qqstepview);
+
+		final QQStepView qs = (QQStepView) findViewById(R.id.qs);
+		qs.setStepMax(4000);
+		qs.setStart(100);
+		qs.setEnd(3000);
+		qs.setDuration(1000);
+		qs.start();
+
+	}
+}
